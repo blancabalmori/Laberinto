1c1178faec55402ed96a20de5deb67110e135858
blancabalmori <91721863+blancabalmori@users.noreply.github.com>
2021-12-08 18:47:19 +0100

    Se q tiene errores, n judgues esta versión

--- Laberinto.py
+++ Laberinto.py
@@ -18,11 +18,12 @@ muro = (
     (4,2),
     (4,3)
 )
-empezar = (0,0)
-acabar = (4,4)
 
 filas = 5 
 columnas = 5
+
+x = 0
+y = 0
 
 def crear_laberinto (filas, columnas):
     laberinto = []
@@ -35,15 +36,20 @@ def crear_laberinto (filas, columnas):
                 linea.append(ficha)
             else:
                 linea.append(cell)
+    print (laberinto)
 
-def movimiento():
-    posición = (0,0)
+def movimiento(x,y):
+    posición = (x,y)
     posicion_nueva = input("¿Hacia donde quiere ir?") 
-    if posicion_nueva == "s" :
-        posición = 
-
-
+    if posicion_nueva == "s":
+        y += 1
+    elif posicion_nueva == "a":
+        x -= 1
+    elif posicion_nueva == "d":
+        x += 1
+    elif posicion_nueva == "w":
+        y -= 1
 
 def laberinto():
-   
-    crear_laberinto
+   crear_laberinto
+   movimiento
